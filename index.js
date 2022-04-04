6c5eb7e6a5f57d1bdcd57ae2d92708c90c9137fb
Digyash kalita <www.digyash.kalita1@gmail.com>
2022-04-04 16:59:38 +0530

    code refactored

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const inputElement = document.getElementById("input");
 const btnElement = document.getElementById("btn");
 const ulElement = document.querySelector("ul");
-const deleteBtnElement = document.createElement("button");
-deleteBtnElement.setAttribute("class", "deleteBtn");
-deleteBtnElement.appendChild(document.createTextNode("X"));
 const form = document.getElementById("form");
 
 //Create New li
 function createNewLi(newToDo) {
+  //create new li
   const newLi = document.createElement("li");
   newLi.appendChild(document.createTextNode(newToDo));
-  newLi.appendChild(deleteBtnElement);
+
+  //create span element
+  const spanElement = document.createElement("span");
+  spanElement.classList.add("actionBtn");
+  newLi.appendChild(spanElement);
+  //create edit btn
+  const editBtn = document.createElement("button");
+  editBtn.appendChild(document.createTextNode("edit"));
+  editBtn.setAttribute("id", "editBtn");
+  spanElement.appendChild(editBtn);
+  //create delete btn
+  const deleteBtn = document.createElement("button");
+  deleteBtn.appendChild(document.createTextNode("delete"));
+  deleteBtn.setAttribute("id", "deleteBtn");
+  spanElement.appendChild(deleteBtn);
   //append  newLi to ul
   ulElement.appendChild(newLi);
+
+  inputElement.value = "";
 }
 
 //Add new To-Do
